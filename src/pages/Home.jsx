fa9d04dd9493514bb4fd5ecbe6eabb2e2aa9073c
AmitGit217 <amit217@yandex.com>
2026-06-07 11:22:47 +0200

    style: home background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,44 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
-
+import { Box, Container, Heading } from "@chakra-ui/react";
+import bgImage from "../assets/main_bg_image.jpg";
 
 export default function Home() {
   return (
-    <Container maxW="container.lg" height="100vh" py="section-y" padding="0" background="url('/src/assets/main_bg_image.jpg')" backgroundSize="cover" backgroundPosition="center">
-        <Box 
-          as="header"
-          fontSize="4xl"
-          fontWeight="bold"
-          textAlign="center"
-          mb="8"
-          padding="0"
-          borderBottom="1.5px solid"
-          borderColor="#FFFFFF33"
-
-        >
-        <Heading
-        as="h1"
-        fontFamily="heading"
-        fontSize="lg"
+    <Container
+      maxW="container.lg"
+      height="100vh"
+      py="section-y"
+      p={0}
+      backgroundImage={`
+        linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+        url('${bgImage}')
+      `}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundAttachment="fixed"
+    >
+      <Box
+        as="header"
+        fontSize="4xl"
         fontWeight="bold"
-        mb={4}
-        textAlign="left"
-        m={0}
-        p="10px"
-        color="white"
+        textAlign="center"
+        mb={8}
+        borderBottom="1.5px solid"
+        borderColor="#ffffff60"
+      >
+        <Heading
+          as="h1"
+          fontFamily="heading"
+          fontSize="lg"
+          fontWeight="bold"
+          m={0}
+          p="10px"
+          color="white"
+          textAlign="left"
         >
-        NewsExplorer
+          NewsExplorer
         </Heading>
-        </Box>
+      </Box>
     </Container>
-  )
+  );
 }
